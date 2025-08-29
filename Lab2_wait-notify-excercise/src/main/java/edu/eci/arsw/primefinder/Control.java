fb01d6053ff364bee18538aa0d2f0dfcbf0be380
elmultiusos <buitragojuans@hotmail.com>
2025-08-29 16:52:20 -0500

    feat: add part 1 of the laboratory

--- Lab2_wait-notify-excercise/src/main/java/edu/eci/arsw/primefinder/Control.java
+++ Lab2_wait-notify-excercise/src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -1,44 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.arsw.primefinder;
 
-/**
- *
- */
 public class Control extends Thread {
-    
+
     private final static int NTHREADS = 3;
-    private final static int MAXVALUE = 30000000;
+    private final static int MAXVALUE = 300000;
     private final static int TMILISECONDS = 5000;
 
     private final int NDATA = MAXVALUE / NTHREADS;
+    private final PrimeFinderThread[] pft;
 
-    private PrimeFinderThread pft[];
-    
+    /* Lock para sincronización */
+    private final Object lock = new Object();
+    /* Variable para controlar el estado de pausa */
+    private volatile boolean paused = false;
+
     private Control() {
         super();
-        this.pft = new  PrimeFinderThread[NTHREADS];
+        this.pft = new PrimeFinderThread[NTHREADS];
 
         int i;
-        for(i = 0;i < NTHREADS - 1; i++) {
-            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA);
+        for (i = 0; i < NTHREADS - 1; i++) {
+            PrimeFinderThread elem = new PrimeFinderThread(i * NDATA, (i + 1) * NDATA, lock, this);
             pft[i] = elem;
         }
-        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1);
+        pft[i] = new PrimeFinderThread(i * NDATA, MAXVALUE + 1, lock, this);
     }
-    
+
     public static Control newControl() {
         return new Control();
     }
 
+    /* Método run del hilo controlador */
     @Override
     public void run() {
-        for(int i = 0;i < NTHREADS;i++ ) {
-            pft[i].start();
+        for (PrimeFinderThread thread : pft) {
+            thread.start();
+        }
+
+        try {
+            while (true) {
+                Thread.sleep(TMILISECONDS);
+
+                synchronized (lock) {
+                    paused = true;
+                    System.out.println("\n⏸ Todos los hilos pausados.");
+                    int total = 0;
+                    for (PrimeFinderThread thread : pft) {
+                        total += thread.getPrimes().size();
+                    }
+                    System.out.println("Primos encontrados hasta ahora: " + total);
+                    System.out.println("Presiona ENTER para continuar...");
+                    lock.wait(); // esperar ENTER
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
-    
+
+    /* Método para reanudar los hilos */
+    public void resumeThreads() {
+        synchronized (lock) {
+            paused = false;
+            lock.notifyAll();
+        }
+    }
+
+    /* Getter del estado de pausa */
+    public boolean isPaused() {
+        return paused;
+    }
 }
--- Lab2_wait-notify-excercise/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ Lab2_wait-notify-excercise/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,12 +1,16 @@
 package edu.eci.arsw.primefinder;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Control control = Control.newControl();
-        
         control.start();
 
+        while (true) {
+            System.in.read(); // esperar ENTER
+            control.resumeThreads();
+        }
     }
-	
 }
